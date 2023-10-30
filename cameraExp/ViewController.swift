e64a600a03c3df0bc3fb97720018cfd55e438432
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2023-10-30 11:48:18 +0600

    Basic camera setup.

--- cameraExp/ViewController.swift
+++ cameraExp/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+fileprivate enum Constants {
+    static let  circleSize: CGFloat = 300
 }
 
+class ViewController: UIViewController {
+    var captureSession: AVCaptureSession!
+    
+    var previewLayer1: AVCaptureVideoPreviewLayer!
+    var previewLayer2: AVCaptureVideoPreviewLayer!
+    var previewLayer3: AVCaptureVideoPreviewLayer!
+
+    var sessionQueue = DispatchQueue(label: "sessionQueue", qos: .userInteractive)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCameraView()
+    }
+}
+
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func setupCameraView() {
+        captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            print("No camera available.")
+            return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+            
+            let output = AVCaptureVideoDataOutput()
+            output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+            captureSession.addOutput(output)
+            
+            previewLayer1 = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer1.videoGravity = .resizeAspectFill
+            previewLayer1.frame = view.bounds
+            view.layer.addSublayer(previewLayer1)
+            
+            previewLayer2 = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer2.videoGravity = .resizeAspectFill
+            previewLayer2.frame = CGRect(x: UIScreen.main.bounds.width / 2 - Constants.circleSize / 2,
+                                         y: UIScreen.main.bounds.height / 2 - Constants.circleSize / 2,
+                                         width: Constants.circleSize,
+                                         height: Constants.circleSize)
+            previewLayer2.cornerRadius = 150
+            view.layer.addSublayer(previewLayer2)
+            
+            sessionQueue.async {
+                self.captureSession.startRunning()
+            }
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // Handle captured video frames here
+        // You can process the frames or display them in the UIImageView
+    }
+}
